78569d98ac4ac1bd391c4fcec5bbfa4abdd799be
vansh123456 <vanshbhatnagar14dev@gmail.com>
2024-11-01 15:48:52 +0530

    query: add getaccountforupdate for preventing double debit of money

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -89,7 +89,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 		//update account ba;lances:
-		account1, err := q.GetAccount(ctx, arg.FromAccountID)
+		account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
 		if err != nil {
 			return err
 		}
@@ -101,7 +101,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		account2, err := q.GetAccount(ctx, arg.ToAccountID)
+		account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
 		if err != nil {
 			return err
 		}
